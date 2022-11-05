3a3e9a3945cca414ea5ebd461f1d53bb22afc221
Nicholas Gorden <ngorden1997@gmail.com>
2022-11-05 07:22:39 -0700

    initial commit

---
+++ www/static/pages/nav.php
@@ -0,0 +1,5 @@
+<li><a href="index.php?page=home">Home</a></li>
+<li><a href="index.php?page=about">About</a></li>
+<li><a href="index.php?page=projects">Projects</a></li>
+<li><a href="linkedin-resume/2022/resume.pdf">Resume</a></li>
+<li><a href="https://linkedin.com/in/nicholas-gorden">LinkedIn</a></li>
